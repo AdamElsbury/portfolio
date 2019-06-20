e1b267e9a2f30097f81fa2d6388b918512e3e0c3
adamelsbury <adam.john.elsbury@gmail.com>
2019-06-20 23:41:47 +0100

    Added mobile hamburger navigation menu

---
+++ js/main.js
@@ -0,0 +1,17 @@
+function showMobileMenu() {
+  var x = document.getElementById('mobilemenu');
+  if (x.style.display === 'none') {
+    x.style.display = 'block';
+  } else {
+    x.style.display = 'none';
+  }
+}
+
+function closeMobileMenu() {
+  var x = document.getElementById('mobilemenu');
+  if (x.style.display === 'block') {
+    x.style.display = 'none';
+  } else {
+    x.style.display = 'block';
+  }
+}
