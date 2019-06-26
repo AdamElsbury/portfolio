c5ea59c26a62cc55a2c1b8ab2c8d573a7c229e72
adamelsbury <adam.john.elsbury@gmail.com>
2019-06-26 16:23:48 +0100

    Removed menu transition, added background overlay when menu active + added JS function to close menu when a child link is clicked

--- js/main.js
+++ js/main.js
@@ -2,8 +2,6 @@ function showMobileMenu() {
   var x = document.getElementById('mobilemenu');
   if (x.style.display === 'none') {
     x.style.display = 'block';
-    x.style.width = '100vw';
-    x.style.height = '100vh';
   } else {
     x.style.display = 'none';
   }
@@ -13,9 +11,14 @@ function closeMobileMenu() {
   var x = document.getElementById('mobilemenu');
   if (x.style.display === 'block') {
     x.style.display = 'none';
-    x.style.width = '0vw';
-    x.style.height = '0vh';
   } else {
     x.style.display = 'block';
+  }
+}
+
+function closeMobileMenuLinkClick() {
+  var x = document.getElementById('mobilemenu');
+  if (x.style.display === 'block') {
+    x.style.display = 'none';
   }
 }
